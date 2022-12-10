ea237088f0324a0d739b7c2e466682ea3c6ade0c
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-10 18:29:04 +0900

    init commit

---
+++ src/test/java/com/tobias/orderservice/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobias.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
