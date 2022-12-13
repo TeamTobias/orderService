084c322234deacd22a4d9c8b7e24fc25db674ca6
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-13 22:50:15 +0900

    OrderInfoResource

--- src/main/java/com/tobias/orderservice/outer/web/OrderInfoResource.java
+++ src/main/java/com/tobias/orderservice/outer/web/OrderInfoResource.java
@@ -4,28 +4,31 @@ import com.tobias.orderservice.outer.dto.OrderInfoPutRequest;
 import com.tobias.orderservice.outer.dto.OrderInfoRequest;
 import com.tobias.orderservice.outer.dto.OrderInfoResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/orderinfo")
 public class OrderInfoResource {
 
 
+	@PostMapping("/v1")
 	public ResponseEntity<String> orderInfoRequest(@RequestBody OrderInfoRequest orderInfoRequest){
 		return null;
 	}
 
+	@GetMapping("/v1/{userid}")
 	public ResponseEntity<List<OrderInfoResponse>> orderInfoResponse(@PathVariable long userid){
 		return null;
 	}
 
+	@PutMapping("/v1")
 	public ResponseEntity<String> orderInfoPutRequest(@RequestBody OrderInfoPutRequest orderInfoPutRequest){
 		return null;
 	}
 
+	@DeleteMapping("/v1/{destinationInfoid}")
 	public ResponseEntity<String> orderInfoDelete(@PathVariable long destinationInfoid){
 		return null;
 	}
