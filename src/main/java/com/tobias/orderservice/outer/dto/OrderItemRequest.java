3497b9a2a41c6d8511fa54e548ef6cafdb1bb2b3
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-13 22:38:45 +0900

    다듬기

--- src/main/java/com/tobias/orderservice/outer/dto/OrderItemRequest.java
+++ src/main/java/com/tobias/orderservice/outer/dto/OrderItemRequest.java
@@ -2,6 +2,8 @@ package com.tobias.orderservice.outer.dto;
 
 import lombok.Data;
 
+import java.util.List;
+
 @Data
 public class OrderItemRequest {
 
@@ -12,7 +14,7 @@ public class OrderItemRequest {
 	private int quantity;
 	private String color;
 	private int size;
-	private int unit_price;
-	public OrderRequest m_OrderRequest;
+	private int unitPrice;
+	private List<OrderRequest> orderRequestList;
 
 }
