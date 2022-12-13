64d33bedad0ce8b41e540d5f80b8b5c3d9d91d65
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-13 22:51:33 +0900

    OrderInfoResource

--- src/main/java/com/tobias/orderservice/outer/web/OrderInfoResource.java
+++ src/main/java/com/tobias/orderservice/outer/web/OrderInfoResource.java
@@ -1,8 +1,10 @@
 package com.tobias.orderservice.outer.web;
 
-import com.tobias.orderservice.outer.dto.OrderInfoPutRequest;
-import com.tobias.orderservice.outer.dto.OrderInfoRequest;
-import com.tobias.orderservice.outer.dto.OrderInfoResponse;
+import com.tobias.orderservice.outer.dto.*;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,21 +15,25 @@ import java.util.List;
 public class OrderInfoResource {
 
 
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
 	@PostMapping("/v1")
 	public ResponseEntity<String> orderInfoRequest(@RequestBody OrderInfoRequest orderInfoRequest){
 		return null;
 	}
 
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OrderInfoResponse.class))))
 	@GetMapping("/v1/{userid}")
 	public ResponseEntity<List<OrderInfoResponse>> orderInfoResponse(@PathVariable long userid){
 		return null;
 	}
 
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
 	@PutMapping("/v1")
 	public ResponseEntity<String> orderInfoPutRequest(@RequestBody OrderInfoPutRequest orderInfoPutRequest){
 		return null;
 	}
 
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
 	@DeleteMapping("/v1/{destinationInfoid}")
 	public ResponseEntity<String> orderInfoDelete(@PathVariable long destinationInfoid){
 		return null;
