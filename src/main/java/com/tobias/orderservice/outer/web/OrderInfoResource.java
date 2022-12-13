42f087bf09009988aa60d026cf25ac793b23000b
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-14 00:07:49 +0900

    swagger api operation 추가

--- src/main/java/com/tobias/orderservice/outer/web/OrderInfoResource.java
+++ src/main/java/com/tobias/orderservice/outer/web/OrderInfoResource.java
@@ -2,6 +2,7 @@ package com.tobias.orderservice.outer.web;
 
 import com.tobias.orderservice.inner.service.OrderInfoService;
 import com.tobias.orderservice.outer.dto.*;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -10,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.annotation.Documented;
 import java.util.List;
 
 @RestController
@@ -20,6 +22,7 @@ public class OrderInfoResource {
 	private final OrderInfoService orderInfoService;
 
 
+	@Operation(summary = "배송 정보 등록")
 	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
 	@PostMapping("/v1")
 	public ResponseEntity<String> orderInfoRequest(@RequestBody OrderInfoRequest orderInfoRequest){
@@ -27,6 +30,7 @@ public class OrderInfoResource {
 		return ResponseEntity.ok("success");
 	}
 
+	@Operation(summary = "배송 정보 조회")
 	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OrderInfoResponse.class))))
 	@GetMapping("/v1/{userid}")
 	public ResponseEntity<List<OrderInfoResponse>> orderInfoResponse(@PathVariable long userid){
@@ -34,6 +38,7 @@ public class OrderInfoResource {
 		return ResponseEntity.ok(orderInfoResponses);
 	}
 
+	@Operation(summary = "배송 정보 수정")
 	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
 	@PutMapping("/v1")
 	public ResponseEntity<String> orderInfoPutRequest(@RequestBody OrderInfoPutRequest orderInfoPutRequest){
@@ -41,6 +46,7 @@ public class OrderInfoResource {
 		return ResponseEntity.ok("OrderInfoPutRequest Success");
 	}
 
+	@Operation(summary = "배송 정보 삭제")
 	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
 	@DeleteMapping("/v1/{destinationInfoid}")
 	public ResponseEntity<String> orderInfoDelete(@PathVariable long destinationInfoid){
--- src/main/java/com/tobias/orderservice/outer/web/OrderResource.java
+++ src/main/java/com/tobias/orderservice/outer/web/OrderResource.java
@@ -4,6 +4,7 @@ import com.tobias.orderservice.inner.service.OrderService;
 import com.tobias.orderservice.outer.dto.OrderRequest;
 import com.tobias.orderservice.outer.dto.OrderResponse;
 import com.tobias.orderservice.outer.dto.ResponseMessage;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -22,6 +23,7 @@ public class OrderResource {
 
     private final OrderService orderService;
 
+    @Operation(summary = "주문 내역 등록")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
     @PostMapping("/v1/orderRequest")
     public ResponseEntity<String> orderRequest(@RequestBody OrderRequest orderRequest) {
@@ -30,6 +32,7 @@ public class OrderResource {
     }
 
 
+    @Operation(summary = "주문 내역 조회")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
     @GetMapping("/v1/orderResponse/{userid}")
     public ResponseEntity<List<OrderResponse>> orderResponse(@PathVariable long userid) {
@@ -37,12 +40,14 @@ public class OrderResource {
     }
 
 
+    @Operation(summary = "주문 내역 삭제")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
     @DeleteMapping("/v1/orderDelete/{orderid}")
     public ResponseEntity<String> orderDelete(@PathVariable long orderid) {
         return null;
     }
 
+    @Operation(summary = "주문 내역 검색")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OrderResponse.class))))
     @GetMapping("/v1/orderSearchRequest/{catalogname}")
     public ResponseEntity<List<OrderResponse>> orderSearchRequest(@RequestBody String catalogname) {
@@ -50,6 +55,7 @@ public class OrderResource {
     }
 
 
+    @Operation(summary = "구매 확정")
     @ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
     @PutMapping("/v1/purchaseConfirmRequest/{orderitemid}")
     public ResponseEntity<String> purchaseConfirmRequest(@PathVariable long orderitemid) {
