b6dda83b75e8de2d3a38b82cdd38349b1145fa79
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-13 22:48:26 +0900

    OrderResource

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM openjdk:17-ea-11-jdk-slim
 VOLUME /tmp
-COPY build/libs/orderService-1.0.jar OrderService.jar
+COPY build/libs/orderService-1.0.jar orderservice.jar
 ENV SPRING_PROFILES_ACTIVE prod
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/OrderService.jar"]
+ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/orderservice.jar"]
--- src/main/java/com/tobias/orderservice/outer/web/OrderResource.java
+++ src/main/java/com/tobias/orderservice/outer/web/OrderResource.java
@@ -2,33 +2,52 @@ package com.tobias.orderservice.outer.web;
 
 import com.tobias.orderservice.outer.dto.OrderRequest;
 import com.tobias.orderservice.outer.dto.OrderResponse;
+import com.tobias.orderservice.outer.dto.ResponseMessage;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*", maxAge = 3600, allowCredentials = "true", exposedHeaders = "Authorization")
+@RequestMapping("/order")
 public class OrderResource {
 
 
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
+	@PostMapping("/v1/orderRequest")
 	public ResponseEntity<String> orderRequest(@RequestBody OrderRequest orderRequest){
 		return null;
 	}
 
+
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
+	@GetMapping("/v1/orderResponse/{userid}")
 	public ResponseEntity<List<OrderResponse>> orderResponse(@PathVariable long userid){
 		return null;
 	}
 
+
+
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
+	@DeleteMapping("/v1/orderDelete/{orderid}")
 	public ResponseEntity<String> orderDelete(@PathVariable long orderid){
 		return null;
 	}
 
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OrderResponse.class))))
+	@GetMapping("/v1/orderSearchRequest/{catalogname}")
 	public ResponseEntity<List<OrderResponse>> orderSearchRequest(@RequestBody String catalogname){
 		return null;
 	}
 
+
+	@ApiResponse(responseCode = "200", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ResponseMessage.class)))
+	@PutMapping("/v1/purchaseConfirmRequest/{orderitemid}")
 	public ResponseEntity<String> purchaseConfirmRequest(@PathVariable long orderitemid){
 		return null;
 	}
