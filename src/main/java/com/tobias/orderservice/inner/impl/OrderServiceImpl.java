702dbccc81c53264ad6a37dfda00f4410505f489
ImKunYoung <ur2kunyoung2@outlook.com>
2022-12-14 11:15:12 +0900

    orderResponse

--- src/main/java/com/tobias/orderservice/inner/impl/OrderServiceImpl.java
+++ src/main/java/com/tobias/orderservice/inner/impl/OrderServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,9 +27,8 @@ public class OrderServiceImpl implements OrderService {
 
 	@Override
 	public List<OrderResponse> orderResponse(long userid) {
-		List<Order> orders = orderRepository.findAllById(userid);
-
-
+		List<Order> orders = orderRepository.findAllByUserid(userid);
+		return orders.stream().map(OrderResponse::new).collect(Collectors.toList());
 	}
 
 	@Override
--- src/main/java/com/tobias/orderservice/outer/repository/OrderRepository.java
+++ src/main/java/com/tobias/orderservice/outer/repository/OrderRepository.java
@@ -4,6 +4,12 @@ package com.tobias.orderservice.outer.repository;
 import com.tobias.orderservice.inner.domain.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Order, Long> {
+
+    List<Order> findAllByUserid(long userid);
+
+
 
 }
